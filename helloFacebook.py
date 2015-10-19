9029816b7bd11411465a3eb7ba0196342883e625
smc1138 <smcogley@yahoo.com>
2015-10-19 13:54:35 -0700

    specifying version 2.2 for Graph API

--- helloFacebook.py
+++ helloFacebook.py
@@ -6,5 +6,6 @@ import requests
 # here: https://developers.facebook.com/tools/explorer/
 access_token = 'CAACEdEose0cBAHHipNcH1rkWkQapRU1cmnF4ZB3vvrJfZBV1JUgPihZCg1ZAgiYNBK86ztDyhjYwp5FTMM4jHyN3CHvGFi1vmmHlORhU9n6JOInz09yN2NAtAIc7I68gdBX1etzZBrFeHwJZCmmp8fzDh1hvPZC0Dnzz7GW92jhWnAAigYML0hsn3RnxXRoZCWCKINWZBsK2QJAZDZD'
 photosedge = 'me/photos'
-graph = facebook.GraphAPI(access_token)
+graph = facebook.GraphAPI(access_token, version='2.2')
+print (graph.version)
 photos = graph.get_object(photosedge)
