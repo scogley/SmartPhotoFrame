b5daaf10c000358744f3d60763c5531e3e81ef5d
smc1138 <smcogley@yahoo.com>
2015-10-21 00:01:30 -0700

    using requests to GET url and print the HTTP status code

--- helloFacebook.py
+++ helloFacebook.py
@@ -16,4 +16,16 @@ photosedge = graph.get_connections(id='me', connection_name='photos')
 #WORKS! 
 #for each object in photosedge print the img source url
 for p in photosedge['data']:
-	print(p['source'])
+	#print(p['source'])
+	url = (p['source'])
+	print(url)
+	r = requests.get(url)
+	print(r.status_code)
+    #image_file = open(path, 'w')
+    #image_file.write(r.content)
+    #image_file.close()
+
+
+#TODO
+# use the Linux framebuffer app to render the image
+# sample: fbi -noverbose -m 640x480 -a -u -t 6 /home/pi/art/**/*
