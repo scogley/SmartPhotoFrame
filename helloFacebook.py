2f6f575fbc5cef7c3f20f80b3ff053245d2395ea
smc1138 <smcogley@yahoo.com>
2015-10-26 15:59:47 -0700

    troubleshooting the open() command and PermissionError Errno 13 permission denied

--- helloFacebook.py
+++ helloFacebook.py
@@ -15,19 +15,36 @@ print (graph.version)
 # By default, reading from the photos edge includes all photos a person has been tagged in.
 photosedge = graph.get_connections(id='me', connection_name='photos')
 
-#WORKS! 
-#for each object in photosedge print the img source url
-for p in photosedge['data']:
-	#print(p['source'])
+
+#for each object in photosedge print the img source url and unique photo id
+for p in photosedge['data']:	
 	url = (p['source'])
 	print(url)
+	id = (p['id'])
+	print(id)
+	filepath = id  + '.jpg'
+	#filepath = '/cygdrive/c/Users/sean/photoframe/' + id  + '.jpg'
+	#filepath = '/home/pi/photoframe/fb/' + id  + '.jpg'
+	print(filepath)
 	r = requests.get(url)
-	print(r.status_code)
-    #image_file = open(path, 'w')
-    #image_file.write(r.content)
-    #image_file.close()
+	print(r.status_code)	
+	try:
+		image_file = open(filepath, 'wb')
+	except IOError:		
+		image_file = open(filepath, 'wb')
+		image_file.write(r.content)
+		image_file.close()
 
+	#image_file = open(filepath)
+        #print ("---> Already have file")
+    	#except IOError:
+        #print ("---> Downloading %s" % url)
+
+    
+    
 
 #TODO
 # use the Linux framebuffer app to render the image
 # sample: fbi -noverbose -m 640x480 -a -u -t 6 /home/pi/art/**/*
+
+# use the links2 browser to render the img urls
