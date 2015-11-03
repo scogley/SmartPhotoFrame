cfd7e6620653d7e7c336dfeddd9747f21d05d379
smc1138 <smcogley@yahoo.com>
2015-11-03 07:33:06 -0800

    changing static url to variable url for file writing

--- helloFacebook.py
+++ helloFacebook.py
@@ -28,7 +28,7 @@ for p in photosedge['data']:
 	print(url)
 	# using urllib
 f = open(filepath,'wb')
-f.write(urllib.urlopen('http://cdn.sstatic.net/stackoverflow/img/sprites.png?v=3c6263c3453b').read())
+f.write(urllib.urlopen(url).read())
 f.close()
 
 
