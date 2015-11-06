266d719f762dd1b158c7084212a723b764aab2f4
smc1138 <smcogley@yahoo.com>
2015-11-06 14:54:52 -0800

    successfully indexing into the properties of a single album object

--- testingGraphAPI.py
+++ testingGraphAPI.py
@@ -11,10 +11,20 @@ graph = facebook.GraphAPI(access_token, version='2.2')
 print (graph.version)
 
 profile = graph.get_object('me')
-#posts = graph.get_connections(profile['id'], 'posts')
+# using graph api explorer: me/albums
 albums = graph.get_connections(profile['id'], 'albums')
 
 for a in albums['data']:
 	id = (a['id'])
 	name = (a['name'])
-	print(id + ' ' + name)
+	print(id + ' ' + name)	
+
+# The above returns the id of each album. 
+# Using the id of iOS Photos to get the album object and return photos field
+# using graph api explorer: 4731781457001?fields=photos
+
+# works!
+iosAlbum = graph.get_object(id='4731781457001?fields=photos')
+for i in iosAlbum['photos']:
+	data = (i[0])
+	id = (data[0])
