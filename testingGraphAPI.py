aac6f9bec411c0cb337f2761bbde610acb827792
smc1138 <smcogley@yahoo.com>
2015-11-06 15:27:33 -0800

    cleaning up commented-out code that is no longer needed. Updating comments on listing id and name of album

--- testingGraphAPI.py
+++ testingGraphAPI.py
@@ -14,12 +14,12 @@ profile = graph.get_object('me')
 # using graph api explorer: me/albums
 albums = graph.get_connections(profile['id'], 'albums')
 
+# lists the id and name of each album
 for a in albums['data']:
 	id = (a['id'])
 	name = (a['name'])
 	print(id + ' ' + name)	
 
-# The above returns the id of each album. 
 # Using the id of iOS Photos to get the album object and return photos field
 # using graph api explorer: 4731781457001?fields=photos
 # works! prints the source img url for each image in the album
@@ -29,27 +29,3 @@ data = (photos['data'])
 for d in data:
 	source = (d['source'])
 	print(source) # this is the source url for the image!
-
-
-# works! prints the source url 
-# iosAlbum = graph.get_object(id='4731781457001?fields=photos')
-# photos = iosAlbum['photos']
-# data = (photos['data'])
-# source = (data[0])['source']
-# print(source) # this is the source url for the image!
-
-
-# works! prints all the objects in photos/data
-# iosAlbum = graph.get_object(id='4731781457001?fields=photos')
-# photos = iosAlbum['photos']
-# data = (photos['data'])
-# id = (data[0])
-# print(id)
-
-
-# works!
-# iosAlbum = graph.get_object(id='4731781457001?fields=photos')
-# for i in iosAlbum['photos']:
-# 	data = (i[0])
-# 	id = (data[0])
-# 	print(id)
